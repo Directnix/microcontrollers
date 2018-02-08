68ff43c2d01a1c3ec5de621e0a18a5a9304c9a45
Nick van Endhoven <nick_vend95@hotmail.com>
2018-02-08 15:27:02 +0100

    Initialize repo

---
+++ microcontrollers/microcontrollers/Track2/interrupt.c
@@ -0,0 +1,18 @@
+/*
+ * microcontrollers.c
+ *
+ * Created: 08-Feb-18 03:24:40 PM
+ * Author : Nick van Endhoven
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
