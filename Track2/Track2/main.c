1f419c1e374af2e08f6391d3fcaf48e9c66e3129
Nick van Endhoven <nick_vend95@hotmail.com>
2018-02-08 16:32:44 +0100

    Added track 2

---
+++ Track2/Track2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Track2.c
+ *
+ * Created: 08-Feb-18 04:31:25 PM
+ * Author : Gebruiker
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
