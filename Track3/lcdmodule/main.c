96c378620cd74c0d1675e50220b9a96a52d01005
Nick van Endhoven <nick_vend95@hotmail.com>
2018-02-22 16:18:23 +0100

    Added track 3 startup

---
+++ Track3/lcdmodule/main.c
@@ -0,0 +1,18 @@
+/*
+ * lcdmodule.c
+ *
+ * Created: 22-Feb-18 04:18:09 PM
+ * Author : Gebruiker
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
