6d766e05aa151b886bf372069826b4c189e4d434
Nick van Endhoven <nick_vend95@hotmail.com>
2018-03-01 13:14:59 +0100

    lcd module

--- Track3/lcdmodule/main.c
+++ Track3/lcdmodule/main.c
@@ -2,15 +2,32 @@
  * lcdmodule.c
  *
  * Created: 22-Feb-18 04:18:09 PM
- * Author : Gebruiker
+ * Author : Nick van Endhoven
  */ 
 
-#include <avr/io.h>
+#define F_CPU 8000000
 
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lcd.h"
 
 int main(void)
 {
-    /* Replace with your application code */
+    lcd_init();
+	
+	lcd_set_cursor(0, 1);
+	lcd_display_text("Hello");
+	
+	lcd_set_cursor(0, 2);
+	lcd_display_text("Ma name Nick!!");
+
+	lcd_set_cursor(8, 1);
+	lcd_display_text("World!");
+	
     while (1) 
     {
     }
